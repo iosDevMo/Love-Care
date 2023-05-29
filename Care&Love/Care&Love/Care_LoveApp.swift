468b445409665561f250e82f217e2b7ca4e33a30
mohamdan <engineerm86@gmail.com>
2023-05-29 14:05:40 +0300

    add image-color assets

---
+++ Care&Love/Care&Love/Care_LoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Care_LoveApp.swift
+//  Care&Love
+//
+//  Created by mohamdan on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Care_LoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
